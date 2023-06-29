d904e84a8d68984c5414f3c4bedb2fd1b807b08f
meetjoysar <99729760+meetjoysar@users.noreply.github.com>
2023-06-29 23:32:24 +0530

    Time: 20 ms (17.89%) | Memory: 7.3 MB (81.24%) - LeetSync

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -3,25 +3,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // int s_sum = 0, t_sum = 0;
-        // for(int i=0; i<s.size(); i++){
-        //     s_sum+=int(s[i]);
+        // if(s.size()==t.size()){
+        //     sort(s.begin(), s.end());
+        //     sort(t.begin(), t.end());
+        //     for(int i=0; i<s.size(); i++){
+        //         if(s[i]!=t[i])
+        //             return false;
         // }
-        // for(int i=0; i<t.size(); i++){
-        //     t_sum+=int(t[i]);
+        // return true;
         // }
-        // if(s_sum == t_sum)
-        //     return true;
         // return false;
-        if(s.size()==t.size()){
-            sort(s.begin(), s.end());
-            sort(t.begin(), t.end());
-            for(int i=0; i<s.size(); i++){
-                if(s[i]!=t[i])
-                    return false;
-        }
-        return true;
-        }
+        if(s.size()!=t.size())
+            return false;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        if(s == t)
+            return true;
         return false;
         
     }
